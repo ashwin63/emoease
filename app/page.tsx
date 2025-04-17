8970c3050665649e559e24bfe69b2c9d724c9a0a
ashwin63 <ashwinsenthil63@gmail.com>
2025-04-17 16:58:13 -0400

    Add voice recording functionality and center bubble

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatBubble from './components/ChatBubble';
 
 export default function Home() {
   return (
-    <div className="fixed inset-0 bg-black w-screen h-screen flex items-center justify-center">
+    <div className="fixed inset-0 bg-black w-screen h-screen">
       <ChatBubble />
     </div>
   );
