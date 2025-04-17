ccad8074820e9c80564f45298c7f4dfecb10806e
ashwin63 <ashwinsenthil63@gmail.com>
2025-04-17 16:50:01 -0400

    Make bubble more visible with test borders

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -2,7 +2,11 @@
 
 export default function ChatBubble() {
   return (
-    <div className="w-40 h-40 bg-blue-500 rounded-full">
-    </div>
+    <div 
+      className="w-64 h-64 bg-gradient-to-b from-white to-blue-500 rounded-full"
+      style={{
+        boxShadow: '0 0 40px rgba(59, 130, 246, 0.5)'
+      }}
+    />
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import ChatBubble from './components/ChatBubble';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <ChatBubble />
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <div className="border-2 border-white p-4">
+        <ChatBubble />
+      </div>
     </div>
   );
 }
